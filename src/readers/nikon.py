e2c61eb3d3a61c1df727b7d206ab5fa069c157f6
Viktor Nikitin <vnikitin@anl.gov>
2024-05-20 17:14:02 -0500

    adding nikon, first commit

---
+++ src/readers/nikon.py
@@ -0,0 +1,84 @@
+import sys
+import pprint
+import xmltodict
+import configparser
+# import pandas as pd
+import pathlib
+# from datetime import datetime
+# from openpyxl.utils.dataframe import dataframe_to_rows
+# from openpyxl import load_workbook
+
+def extract_meta_from_config(file_name):
+    meta_dict = {}
+    # Exact meta data from config file (pca)
+    try:
+        with open(file_name, encoding='latin-1') as f:
+            config = configparser.ConfigParser(interpolation=None)
+            config.read_file(f)   
+
+        sections = config.sections()
+        meta_dict = {i: {i[0]: i[1] for i in config.items(i)} for i in config.sections()}
+        # for key in meta_dict:
+        #     print(key, meta_dict[key])
+    except FileNotFoundError:
+        print("ERROR: %s is missing. Looking for a _rar file" % file_name)
+        fname        = pathlib.Path(file_name)
+        fname_suffix = fname.suffix
+        file_name_rar  = fname.with_name(file_name.stem + '_rar').with_suffix(fname_suffix)
+        print('Found %s file' % file_name_rar)
+        try:
+            with open(file_name_rar, encoding='latin-1') as f:
+                config = configparser.ConfigParser()
+                config.read_file(f)   
+
+            sections = config.sections()
+            meta_dict = {i: {i[0]: i[1] for i in config.items(i)} for i in config.sections()}
+        except:
+            print("ERROR: %s is also missing" % file_name_rar)
+            exit()
+    print('***********************')
+    print(file_name)
+    pprint.pprint(meta_dict)
+    return meta_dict
+
+def extract_meta_from_xml(file_name):
+    # Extract meta data from dtxml file
+    with open(file_name, encoding='latin-1') as f:
+        xml_content= f.read()
+
+    xml_dict = xmltodict.parse(xml_content)
+    print('***********************')
+    print(file_name)
+    pprint.pprint(xml_dict)
+    return xml_dict
+
+def main(args):
+
+    if len(sys.argv) == 1:
+        print ('ERROR: Must provide the path to a run-file folder as the argument')
+        print ('Example:')
+        print ('        python nikon.py /data/sample_name/')
+        sys.exit(1)
+    else:
+
+        file_name   = sys.argv[1]
+        p = pathlib.Path(file_name)
+        if p.is_dir():
+            p = pathlib.Path(file_name) #.joinpath(p.stem)
+            list_parts = list(p.parts)
+            fname = list_parts[-1][:-22]
+            list_parts.append(fname)
+            p = p.joinpath(*list_parts)
+
+            file_name_ctinfo_xml = p.with_suffix('.ctinfo.xml')
+            file_name_ctprofile_xml = p.with_suffix('.ctprofile.xml')
+            file_name_xtekct = p.with_suffix('.xtekct')
+        else:
+            print('ERROR: %s does not exist' % p)
+            sys.exit(1)
+    my_xtekct_dict        = extract_meta_from_config(file_name_xtekct)
+    my_ctinfo_xml_dict    = extract_meta_from_xml(file_name_ctinfo_xml)
+    my_ctprofile_xml_dict = extract_meta_from_xml(file_name_ctprofile_xml)
+
+if __name__ == "__main__":
+   main(sys.argv)
